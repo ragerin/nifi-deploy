5891f689603b539af553b235fabd2655296c1038
Mads Hovgård Jakobsen <mads.jakobsen@skat.dk>
2018-02-16 11:29:39 +0100

    added docstrings, cleaned up a bit

--- nifi-deploy/nifi-deploy.py
+++ nifi-deploy/nifi-deploy.py
@@ -12,14 +12,13 @@ def export_function(args):
     uuid            = args.uuid    
     name            = args.name
     description     = args.description
-    filename        = args.filename                # 'C:\\Temp\\template_test.xml'
+    filename        = args.filename
     keep_template   = args.keep_template
 
-    n = nifi.NifiInstance(nifi_host, username=username, password=password) # 'https://sktudv01hdf01.ccta.dk:9091/nifi-api'
-
     try:
+        n = nifi.NifiInstance(nifi_host, username=username, password=password)
+    
         process_group = canvas.get_process_group(uuid, 'id')
-        
         template = n.create_template(
             pg_id=uuid,
             name=name,
@@ -27,13 +26,16 @@ def export_function(args):
             ).template        
 
         xml = n.export_template(template.id, filename)
-        if not filename: # write to stdout instead
+        if not filename and args.verbose: # Write to stdout
             print(xml)
 
     finally:
         if not args.keep_template:
             temp = templates.get_template_by_name(name)
             r = templates.delete_template(temp.id)
+            if args.verbose:
+                print(r.status_code, r.content.decode())
+
 
 def import_function(args):
     nifi_host       = args.nifi_host
@@ -43,13 +45,21 @@ def import_function(args):
 
     n = nifi.NifiInstance(nifi_host, username=username, password=password)
 
-    n.import_template(args.filename)
+    r = n.import_template(args.filename)
+    if args.verbose:
+        print(r.status_code, r.content.decode())
 
 
+### argparse setup
 parser = argparse.ArgumentParser(prog='nifi-deploy')
+parser.add_argument('-v', '--verbose', action='store_true')
 subparsers = parser.add_subparsers(title='Actions', help='Append --help to view action specific help')
 
-parser_export = subparsers.add_parser('export')
+
+example_usage = """EXAMPLE USAGE:
+nifi-deploy export -n https://nifihost:9090 -u john -p badpractice 0a7361fd-015f-1000-ffff-ffffd2cbc7a7 my_great_template -d template description -f c:\\temp\\my_great_template_export.xml
+"""
+parser_export = subparsers.add_parser('export', epilog=example_usage)
 parser_export.add_argument('uuid', help='UUID of a process group to export as a template XML')
 parser_export.add_argument('name', help='Name of exported template - also the name internally if `--keep_template` is included')
 parser_export.add_argument('-n', '--nifi_host', help='Nifi host (typically http://<nifihost>:8080), else using environment variable `NIFI_HOST`')
@@ -60,12 +70,17 @@ parser_export.add_argument('-f', '--filename', help='Path to save exported XML t
 parser_export.add_argument('-k', '--keep_template', action='store_true', default=False, help='Keep the template in Nifi after export, else exclusively export XML and delete the temporarily instantiated template')
 parser_export.set_defaults(func=export_function)
 
-parser_import = subparsers.add_parser('import')
+
+example_usage = """EXAMPLE USAGE:
+nifi-deploy import -n https://nifihost:9090 -u john -p badpractice c:\\temp\\my_great_template_export.xml
+"""
+parser_import = subparsers.add_parser('import', epilog=example_usage)
 parser_import.add_argument('filename', help='Path to template XML file for uploading to Nifi')
 parser_import.add_argument('-n', '--nifi_host', help='Nifi host (typically http://<nifihost>:8080), else using environment variable `NIFI_HOST`')
 parser_import.add_argument('-u', '--username', help='Nifi username, else using environment variable `NIFI_USERNAME`')
 parser_import.add_argument('-p', '--password', help='Nifi password, else using environment variable `NIFI_PASSWORD`')
 parser_import.set_defaults(func=import_function)
+
 
 args = parser.parse_args()
 if getattr(args, 'func', None):
--- nifi-deploy/nifi.py
+++ nifi-deploy/nifi.py
@@ -1,3 +1,9 @@
+"""
+Provides a NifiInstance class facilitating easy to use
+methods utilizing the NiPyApi (https://github.com/Chaffelson/nipyapi)
+wrapper library.
+"""
+
 import os
 import getpass
 import requests
@@ -5,21 +11,26 @@ import requests
 from nipyapi import nifi, config, templates, canvas
 
 # Disable urllib3 certificate warnings
-from urllib3 import disable_warnings
-disable_warnings()
-
-
-
-
+requests.packages.urllib3.disable_warnings()
 
 
 class NifiInstance:
+    """ The NifiInstance class facilitating easy to use
+    methods utilizing the NiPyApi (https://github.com/Chaffelson/nipyapi)
+    wrapper library.
+
+    Arguments:
+        url         (str): Nifi host url, defaults to environment variable `NIFI_HOST`.
+        username    (str): Nifi username, defaults to environment variable `NIFI_USERNAME`.
+        password    (str): Nifi password, defaults to environment variable `NIFI_PASSWORD`.
+        verify_ssl  (bool): Whether to verify SSL connection - UNUSED as of now.
+
+    """
     def __init__(self, url=None, username=None, password=None, verify_ssl=False):
-        self.username = username
         config.nifi_config.host = self._get_url(url)
         config.nifi_config.verify_ssl = verify_ssl
-
-        self.authenticate(username, password)
+        config.nifi_config.username = username
+        self._authenticate(username, password)
 
 
     def _get_url(self, url):
@@ -35,13 +46,12 @@ class NifiInstance:
         return url
 
 
-    def authenticate(self, username=None, password=None):
-        # TODO: Attempt get username/password from env vars
+    def _authenticate(self, username=None, password=None):
         if not username:
             try:
-                self.username = os.environ['NIFI_USERNAME']
+                config.nifi_config.username = os.environ['NIFI_USERNAME']
             except KeyError:
-                self.username = input('Username: ')
+                config.nifi_config.username = input('Username: ')
 
         if not password:
             try:
@@ -50,63 +60,83 @@ class NifiInstance:
                 password = getpass.getpass('Password: ')
 
         try:
-            access_token = nifi.AccessApi().create_access_token(username=username,password=password)
-            config.nifi_config.api_key[username] = access_token
-            config.nifi_config.api_client = nifi.ApiClient(header_name='Authorization', header_value='Bearer {}'.format(access_token))
-            
+            access_token = nifi.AccessApi().create_access_token(username=username,password=password)            
         except nifi.rest.ApiException as e:
             print("Exception when calling AccessApi->create_access_token: %s\n" % e)
 
+        config.nifi_config.api_key[username] = access_token
+        config.nifi_config.api_client = nifi.ApiClient(header_name='Authorization', header_value='Bearer {}'.format(access_token))        
+
 
     def create_template(self, pg_id, name, desc=''):
+        """ Create a template from process group id.
+
+        Arguments:
+            pg_id   (str): Process group ID to create the template from.
+            name    (str): Name of the template to create.
+            desc    (str): Optional, description of the template to create.
+        
+        Returns:
+            nipyapi.nifi.TemplateEntity 
+        """
+
         template = templates.create_template(
             pg_id=pg_id,
             name=name,
             desc=desc
             )
+
         return template
     
 
     def export_template(self, template_id, file_path=None):
+        """ Export a template as XML, and optionally write it to a file.
+
+        Arguments:
+            template_id (str): ID of the template to export.
+            file_path   (str): Optional, path of file to write the XML to.
+        
+        Returns:
+            requests.Response
+        """
+
         # This implementation is a workaround for
         # https://github.com/Chaffelson/nipyapi/issues/42
-        from urllib3 import PoolManager, util
-        con = PoolManager()
-        headers = {'Authorization':'Bearer {}'.format(config.nifi_config.api_key[self.username])}
+        headers = {'Authorization':'Bearer {}'.format(config.nifi_config.api_key[config.nifi_config.username])}
         url = config.nifi_config.host + '/templates/' + template_id + '/download'
-        response = con.request('GET', url, headers=headers, preload_content=False)
-
+        response = requests.get(url, headers=headers, verify=config.nifi_config.verify_ssl)
         if file_path:
             file_path = file_path.strip('\'"')
             if os.path.isdir(file_path):
                 file_path = os.path.join(file_path, 'template.xml')
             with open(file_path, 'w') as fd:
-                fd.write(response.data.decode())
+                fd.write(response.content.decode())
 
-        return response.data.decode()
+        return response
     
 
-    def import_template(self, file_path=None):
-        canvas_id = canvas.get_root_pg_id()
+    def import_template(self, file_path):
+        """ Imports a template XML into Nifi's template store.
+
+        Arguments:
+            file_path   (str): Path of the XML file to import into Nifi as a template.
+        
+        Returns:
+            requests.Response
+        """
+
+        # This implementation is a workaround for
+        # https://github.com/Chaffelson/nipyapi/issues/42
         file_path = file_path.strip('\'"')
         with open(file_path, 'r') as fd:
             filename = os.path.basename(fd.name)
             filedata = fd.read()
             
             files = {'template': (filename, filedata, 'text/xml')}
-            headers = {'Authorization': 'Bearer {}'.format(config.nifi_config.api_key[self.username])}
+            headers = {'Authorization': 'Bearer {}'.format(config.nifi_config.api_key[config.nifi_config.username])}
 
+            canvas_id = canvas.get_root_pg_id()            
             url = config.nifi_config.host + '/process-groups/' + canvas_id + '/templates/upload'
-
             response = requests.post(url, headers=headers, files=files, verify=config.nifi_config.verify_ssl)
 
-            print(response.text)
-
-            # print(response)
-            # print(response.text)
-            # print(response.data)
-
-            # response = con.request('POST', url, headers=headers, field={
-            #     'filefield':(filename, filedata, 'text/xml')
-            # })
-        
+        return response
